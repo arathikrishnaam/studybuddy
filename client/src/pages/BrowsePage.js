1354a494eea3fa0e7384231d0cf10f92eb4b9a76
arathikrishnaam <krishnaamarathi@gmail.com>
2025-08-10 21:30:15 +0530

    feat: Added ProfilePage.js and BrowsePage.js

---
+++ client/src/pages/BrowsePage.js
@@ -0,0 +1,10 @@
+import ProfileList from '../components/ProfileList';
+function BrowsePage() {
+  return (
+    <div>
+      <h2>Browse Study Buddies</h2>
+      <ProfileList />
+    </div>
+  );
+}
+export default BrowsePage;
---
+++ client/src/pages/ProfilePage.js
@@ -0,0 +1,10 @@
+import ProfileForm from '../components/ProfileForm';
+function ProfilePage() {
+  return (
+    <div>
+      <h2>Create Study Profile</h2>
+      <ProfileForm />
+    </div>
+  );
+}
+export default ProfilePage;
